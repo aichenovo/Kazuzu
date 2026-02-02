f7501f5b70cccc004cd59279232f8eff82666769
aichenovo <3809933253@qq.com>
2026-02-02 11:31:56 +0800

    Add files via upload

--- lib/pages/video/video_controller.dart
+++ lib/pages/video/video_controller.dart
@@ -11,7 +11,7 @@ import 'package:kazumi/utils/logger.dart';
 import 'package:window_manager/window_manager.dart';
 import 'package:kazumi/modules/bangumi/episode_item.dart';
 import 'package:kazumi/modules/comments/comment_item.dart';
-import 'package:kazumi/request/bangumi.dart';
+import 'package:kazumi/request/tmdb.dart';
 import 'package:dio/dio.dart';
 
 part 'video_controller.g.dart';
@@ -37,10 +37,6 @@ abstract class _VideoPageController with Store {
   /// 全屏状态
   @observable
   bool isFullscreen = false;
-
-  /// 评论正序或倒序
-  @observable
-  bool isCommentsAscending = false;
 
   /// 画中画状态
   @observable
@@ -91,24 +87,24 @@ abstract class _VideoPageController with Store {
 
   Future<void> queryBangumiEpisodeCommentsByID(int id, int episode) async {
     episodeCommentsList.clear();
-    episodeInfo = await BangumiHTTP.getBangumiEpisodeByID(id, episode);
-    await BangumiHTTP.getBangumiCommentsByEpisodeID(episodeInfo.id)
-        .then((value) {
-      episodeCommentsList.addAll(value.commentList);
-    });
-    if (!isCommentsAscending) {
-      episodeCommentsList
-          .sort((a, b) => b.comment.createdAt.compareTo(a.comment.createdAt));
-    } else {
-      episodeCommentsList
-          .sort((a, b) => a.comment.createdAt.compareTo(b.comment.createdAt));
+    final String mediaType = bangumiItem.type == 1 ? 'movie' : 'tv';
+    final reviews = await TMDBHTTP.getReviews(id, mediaType: mediaType);
+    for (final r in reviews) {
+      episodeCommentsList.add(
+        EpisodeCommentItem(
+          comment: EpisodeComment(
+            user: r.user,
+            comment: r.comment.comment,
+            createdAt: r.comment.updatedAt,
+          ),
+          replies: const [],
+        ),
+      );
     }
-    KazumiLogger().i(
-        'VideoPageController: loaded comments list length ${episodeCommentsList.length}');
+    KazumiLogger().i('VideoPageController: loaded comments list length ${episodeCommentsList.length}');
   }
 
-  Future<void> queryRoads(String url, String pluginName,
-      {CancelToken? cancelToken}) async {
+  Future<void> queryRoads(String url, String pluginName, {CancelToken? cancelToken}) async {
     if (cancelToken != null) {
       _queryRoadsCancelToken?.cancel();
       _queryRoadsCancelToken = cancelToken;
@@ -123,23 +119,11 @@ abstract class _VideoPageController with Store {
     roadList.clear();
     for (Plugin plugin in pluginsController.pluginList) {
       if (plugin.name == pluginName) {
-        roadList.addAll(
-            await plugin.querychapterRoads(url, cancelToken: cancelToken));
+        roadList.addAll(await plugin.querychapterRoads(url, cancelToken: cancelToken));
       }
     }
-    KazumiLogger()
-        .i('VideoPageController: road list length ${roadList.length}');
-    KazumiLogger().i(
-        'VideoPageController: first road episode count ${roadList[0].data.length}');
-  }
-
-  void toggleSortOrder() {
-    isCommentsAscending = !isCommentsAscending;
-    episodeCommentsList.sort(
-      (a, b) => isCommentsAscending
-          ? a.comment.createdAt.compareTo(b.comment.createdAt)
-          : b.comment.createdAt.compareTo(a.comment.createdAt),
-    );
+    KazumiLogger().i('VideoPageController: road list length ${roadList.length}');
+    KazumiLogger().i('VideoPageController: first road episode count ${roadList[0].data.length}');
   }
 
   void cancelQueryRoads() {
