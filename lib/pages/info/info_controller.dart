abc6324a88d1d8f3367f02f419c7eb75b56ac4d5
aichenovo <3809933253@qq.com>
2026-02-02 11:32:49 +0800

    Add files via upload

--- lib/pages/info/info_controller.dart
+++ lib/pages/info/info_controller.dart
@@ -3,7 +3,7 @@ import 'package:kazumi/modules/bangumi/bangumi_item.dart';
 import 'package:kazumi/pages/collect/collect_controller.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:kazumi/modules/search/plugin_search_module.dart';
-import 'package:kazumi/request/bangumi.dart';
+import 'package:kazumi/request/tmdb.dart';
 import 'package:mobx/mobx.dart';
 import 'package:kazumi/utils/logger.dart';
 import 'package:kazumi/modules/comments/comment_item.dart';
@@ -38,42 +38,55 @@ abstract class _InfoController with Store {
 
   Future<void> queryBangumiInfoByID(int id, {String type = "init"}) async {
     isLoading = true;
-    await BangumiHTTP.getBangumiInfoByID(id).then((value) {
-      if (value != null) {
-        if (type == "init") {
-          bangumiItem = value;
-        } else {
-          bangumiItem.summary = value.summary;
-          bangumiItem.tags = value.tags;
-          bangumiItem.rank = value.rank;
-          bangumiItem.airDate = value.airDate;
-          bangumiItem.airWeekday = value.airWeekday;
-          bangumiItem.alias = value.alias;
-          bangumiItem.ratingScore = value.ratingScore;
-          bangumiItem.votes = value.votes;
-          bangumiItem.votesCount = value.votesCount;
-        }
-        collectController.updateLocalCollect(bangumiItem);
-        isLoading = false;
+    try {
+      final String primaryMediaType = bangumiItem.type == 1 ? 'movie' : 'tv';
+      final String fallbackMediaType =
+          primaryMediaType == 'tv' ? 'movie' : 'tv';
+      BangumiItem? value = await TMDBHTTP.getDetails(
+        id,
+        mediaType: primaryMediaType,
+      );
+      value ??= await TMDBHTTP.getDetails(
+        id,
+        mediaType: fallbackMediaType,
+      );
+      if (value == null) return;
+
+      if (type == "init") {
+        bangumiItem = value;
+      } else {
+        bangumiItem.summary = value.summary;
+        bangumiItem.tags = value.tags;
+        bangumiItem.rank = value.rank;
+        bangumiItem.airDate = value.airDate;
+        bangumiItem.airWeekday = value.airWeekday;
+        bangumiItem.alias = value.alias;
+        bangumiItem.ratingScore = value.ratingScore;
+        bangumiItem.votes = value.votes;
+        bangumiItem.votesCount = value.votesCount;
       }
-    });
+      collectController.updateLocalCollect(bangumiItem);
+    } finally {
+      isLoading = false;
+    }
   }
 
   Future<void> queryBangumiCommentsByID(int id, {int offset = 0}) async {
     if (offset == 0) {
       commentsList.clear();
     }
-    await BangumiHTTP.getBangumiCommentsByID(id, offset: offset).then((value) {
-      commentsList.addAll(value.commentList);
-    });
+    final String mediaType = bangumiItem.type == 1 ? 'movie' : 'tv';
+    final list =
+        await TMDBHTTP.getReviews(id, mediaType: mediaType, offset: offset);
+    commentsList.addAll(list);
     KazumiLogger().i('InfoController: loaded comments list length ${commentsList.length}');
   }
 
   Future<void> queryBangumiCharactersByID(int id) async {
     characterList.clear();
-    await BangumiHTTP.getCharatersByBangumiID(id).then((value) {
-      characterList.addAll(value.charactersList);
-    });
+    final String mediaType = bangumiItem.type == 1 ? 'movie' : 'tv';
+    final list = await TMDBHTTP.getCharacters(id, mediaType: mediaType);
+    characterList.addAll(list);
     Map<String, int> relationValue = {
       '主角': 1,
       '配角': 2,
@@ -94,9 +107,9 @@ abstract class _InfoController with Store {
 
   Future<void> queryBangumiStaffsByID(int id) async {
     staffList.clear();
-    await BangumiHTTP.getBangumiStaffByID(id).then((value) {
-      staffList.addAll(value.data);
-    });
+    final String mediaType = bangumiItem.type == 1 ? 'movie' : 'tv';
+    final list = await TMDBHTTP.getStaffs(id, mediaType: mediaType);
+    staffList.addAll(list);
     KazumiLogger().i('InfoController: loaded staff list length ${staffList.length}');
   }
 }
