a6577f418e52ca9530d833880a0849956bbcff91
aichenovo <3809933253@qq.com>
2026-03-16 10:28:03 +0800

    Update about_module.dart

--- lib/pages/about/about_module.dart
+++ lib/pages/about/about_module.dart
@@ -16,7 +16,7 @@ class AboutModule extends Module {
       "/license",
       child: (_) => const LicensePage(
         applicationName: 'Sad',
-        applicationVersion: Api.version,
+        applicationVersion: 1.0.0,
         applicationLegalese: '开源许可证',
       ),
     );
