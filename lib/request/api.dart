3f5154a25f8841a37020f1f4bbf28d272d366168
aichenovo <3809933253@qq.com>
2026-03-23 10:50:17 +0800

    Update api.dart

--- lib/request/api.dart
+++ lib/request/api.dart
@@ -10,7 +10,7 @@ class Api {
   /// 图标作者
   static const String iconUrl = "";
   /// 规则仓库
-  static const String pluginShop = 'https://raw.githubusercontent.com/Predidit/KazumiRules/main/';
+  static const String pluginShop = '';
   /// 在线升级
   static const String latestApp =
       ''; 
